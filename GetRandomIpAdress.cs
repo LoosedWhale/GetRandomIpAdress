ca2b4f0a98b5a5f567ae26e3630282aebaef5b65
LoosedWhale <81869656+LoosedWhale@users.noreply.github.com>
2024-01-18 10:27:17 +0100

    Create GetRandomIpAdress.cs

---
+++ GetRandomIpAdress.cs
@@ -0,0 +1,5 @@
+ public static string GetRandomIpAddress()
+ {
+     var random = new Random();
+     return $"{random.Next(1, 255)}.{random.Next(0, 255)}.{random.Next(0, 255)}.{random.Next(0, 255)}";
+ }
